361156252ae7c82a4c3e8044da94134e44f9afa7
Pavel Pontus <pavel.pontus@effective-soft.com>
2018-03-05 14:00:31 +0300

    revert back sliding accordion

--- Web/src/app/pages/home/home-page/home-page.component.ts
+++ Web/src/app/pages/home/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ProductGroupModel} from "../../../models/product-group.model";
 import {ProductItemModel} from "../../../models/product-item.model";
 import {ProductGroupService} from "../../../services/product-group.service";
+import {NgxCarousel, NgxCarouselStore} from "ngx-carousel";
 
 @Component({
   selector: 'app-home-page',
@@ -11,6 +12,7 @@ import {ProductGroupService} from "../../../services/product-group.service";
 export class HomePageComponent implements OnInit {
 
   groupList: ProductGroupModel[];
+  introductionList: any[];
 
   constructor(private productGroupService: ProductGroupService) { }
 
@@ -18,6 +20,12 @@ export class HomePageComponent implements OnInit {
 
     this.groupList = this.productGroupService.getList();
 
+    this.introductionList =  [
+      {imageUrl: 'https://img1.liveinternet.ru/images/attach/c/6/92/919/92919523_large_d2be8d7272cf.jpg', text: 'hello'},
+      {imageUrl: 'https://img1.liveinternet.ru/images/attach/c/10/110/218/110218457_large_R_RRRRRR_2.jpg', text: 'hello'},
+      {imageUrl: 'https://img1.liveinternet.ru/images/attach/c/6/89/863/89863303_large_000070.jpg', text: 'hello'},
+      {imageUrl: 'http://3.bp.blogspot.com/-cB1M4_GdhCg/Ucl3BQvhyII/AAAAAAAAz8A/nH6beKrO8DA/s1600/Stephanie_Birdsall_17.jpg', text: 'hello'}
+    ];
   }
 
 }
