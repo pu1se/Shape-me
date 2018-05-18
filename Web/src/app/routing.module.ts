b2d2a7040e1285b9328175e669882a466cd556e3
Pavel Pontus <pavel.pontus@effective-soft.com>
2018-05-18 11:19:12 +0300

    switched router

--- Web/src/app/routing.module.ts
+++ Web/src/app/routing.module.ts
@@ -15,7 +15,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
+    imports: [RouterModule.forRoot(appRoutes, {useHash: false})],
     exports: [RouterModule]
 })
 
